df22b70c349a669081b7abb36dde5370352db965
odradeck <odradeck@gmail.com>
2012-12-29 07:21:49 +0900

    Initial empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tnt::Application.config.secret_token = 'd6d29d778350ac740ce757ced574f1a58c287982bd2c6174c5c4e5b179e8e0b7dddfe33cbc225bdb55344d3fa3b14b4fcd2d7897748ee61eec76796ba4de64ed'
