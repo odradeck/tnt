45445931a92488df729b4065254e8712cd68ee01
ssp <ssp@localhost>
2012-12-29 17:43:30 +0900

    123

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,8 +4,6 @@ class RegistrationsController < Devise::RegistrationsController
     @user = User.find(current_user.id)
     email_changed = @user.email != params[:user][:email]
     password_changed = !params[:user][:password].empty?
-    
-     
 
     successfully_updated = if email_changed or password_changed
       @user.update_with_password(params[:user])
